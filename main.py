71b8c1f8403065bdf64ed76e5958ca4cc9e0421a
Kislota <porocenok670@gmail.com>
2026-08-17 18:31:26 +0300

    Update 2

--- main.py
+++ main.py
@@ -2,15 +2,55 @@ from kivymd.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
 from kivy import platform
-from kivy.core.window import Window
+from kivy.core.window import Window, Clock
 from kivymd.uix.button import MDIconButton
-
+from kivy.metrics import dp, sp
+from kivymd.uix.widget import MDWidget
+FPS = 60
+BULLET_SPEED = dp(10)
+SHIP_SPEED = dp(5)
 
 class MainScreen(MDScreen):
     ...
 
 
 class GameScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs) 
+        Clock.schedule_interval(self.update, 1/FPS)
+        
+        self.evetkey = {}
+        self.catridge = []
+        
+    def update(self, dt):
+        for key,in self.evetkeys:
+            if self.evetkeys[key] == True:
+                if key == 'left':
+                    self.moveLeft()
+                if key == "rigth":
+                    self.moveRight()
+                if key == "shot":
+                    self.shot()
+                    self.eventkeys[key] = False
+                    
+        for bullet in self.cartridge:
+            bullet.poss[1] += BULLET_SPEED
+            
+    def pressKey(self, key):
+        self.eventkeys[key] = True
+    def releaseKey(self, key):
+        self.eventkeys[key] = False
+    def moveLeft(self):
+        self.ids.ship.pos[0] -= SHIP_SPEED
+    def moveRight(self):
+            self.ids.ship.pos[0] += SHIP_SPEED
+    def shot(self):
+        shot = Shot(pos=(self.ids.ship.center_x, self.ids.ship.top))
+        self.catridge.append(shot)
+        self.ids.front.add_widget(shot)
+        
+                
+class Shot(MDWidget):
     ...
 
 class SettingsScren(MDScreen):
